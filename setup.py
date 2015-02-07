07122132b06a907a118b6bbd4c58c1d0db313006
J.M. Dana <jmdana@gmail.com>
2015-02-07 18:48:49 +0000

    typos

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 # Copyright (c) 2015 Jose M. Dana
 #
-# This file is part of memprof.
+# This file is part of pyfindit.
 #
-# memprof is free software: you can redistribute it and/or modify
+# pyfindit is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation (version 2 of the License only).
 #
-# memprof is distributed in the hope that it will be useful,
+# pyfindit is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with memprof.  If not, see <http://www.gnu.org/licenses/>.
+# along with pyfindit.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
 import sys
