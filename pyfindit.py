90b93298d3300ec3b737201f176ee404545ccd77
J.M. Dana <jmdana@gmail.com>
2015-02-07 18:18:03 +0000

    better RE

--- pyfindit.py
+++ pyfindit.py
@@ -9,8 +9,8 @@ NO_COMMENT = r"(?!.*\s*[#]\s*)"
 
 CLASS = r"%s(class)(\s*)({keyword})(\s*\(.*$)" % NO_COMMENT
 DEF = r"%s(def)(\s*)({keyword})(\s*\(.*$)" % NO_COMMENT
-INIT = r"%s({keyword})(\s*=.*$)" % NO_COMMENT
-USE = r"%s(.*[\s\(\[\.])({keyword})(?!\s*[=\w].*)(.*$)" % NO_COMMENT
+INIT = r"%s(\s*)({keyword})(\s*)(=)(.*$)" % NO_COMMENT
+USE = r"%s(.*[\s\(\[\.,])({keyword})(?!\s*[=\w].*)(.*$)" % NO_COMMENT
 
 COLORS = {
     "red": 31,
