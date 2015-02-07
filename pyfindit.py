be27488b2acad558c3ca8c0cc16f3ff5b9ff22c9
J.M. Dana <jmdana@gmail.com>
2015-02-07 17:16:31 +0000

    initial commit

---
+++ pyfindit.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import re
+import os
+import argparse
+
+DEF = r"(^\s*[^#]def\s*)({keyword})(\s*\(.*$)"
+INIT = r"(^\s*[^#])({keyword})(\s*=.*$)"
+USE = r"(^\s*[^#].*[\s\(\[\.])({keyword})(?!\s*=.*)(.*$)"
+
+def highlight(string):
+    bold = "\033[1m"
+    green = "\033[32m"
+    reset = "\033[0m"
+
+    return bold + green + string + reset
+
+def fmt_match(fname, idx, keyword, match):
+    out = "%s :%d\t" % (fname, idx)
+
+    for g in match.groups():
+        if g == keyword:
+            out += highlight(g)
+        else:
+            out += g
+
+    return out
+
+def search_file(fname, pattern, keyword):
+    f = open(fname, "r")
+
+    pattern_re = re.compile(pattern.format(keyword=keyword))
+
+    for idx, line in enumerate(f, start=1):
+        m = re.match(pattern_re, line)
+        if m:
+            print(fmt_match(fname, idx, keyword, m))
+
+    f.close()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("keyword", type=str, nargs=1, help="the keyword to find")
+    parser.add_argument("path", type=str, nargs="?", default=".", help="the path")
+
+    args, unknown = parser.parse_known_args()
+
+    for root, dirs, fnames in os.walk(args.path):
+        for fname in [x for x in fnames if x.endswith(".py")]:
+            full_path = os.path.join(root, fname)
+            search_file(full_path, INIT, args.keyword[0])
+            search_file(full_path, DEF, args.keyword[0])
+            search_file(full_path, USE, args.keyword[0])
+
