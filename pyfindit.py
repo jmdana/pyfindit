fcd6086810ee39347e139af47e8e9f18a5babeed
J.M. Dana <jmdana@gmail.com>
2015-02-08 17:30:55 +0000

    a better help formatting

--- pyfindit.py
+++ pyfindit.py
@@ -53,12 +53,12 @@ def search_pattern(fname, patterns, keyword):
     f.close()
 
 def main():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument("-c", "--class", dest="c", action="store_true", help="search for classes")
-    parser.add_argument("-d", "--def", dest="d", action="store_true", help="search for functions")
-    parser.add_argument("-v", "--variable", dest="v", action="store_true", help="search for variable assignment.")
-    parser.add_argument("-o", "--other", dest="o", action="store_true", help="search for other apperances.")
+    parser.add_argument("-c", "--class", dest="c", action="store_true", help="search for class names only.")
+    parser.add_argument("-d", "--def", dest="d", action="store_true", help="search for function names only.")
+    parser.add_argument("-v", "--variable", dest="v", action="store_true", help="search for variable assignments only.")
+    parser.add_argument("-o", "--other", dest="o", action="store_true", help="search for other apperances only.")
 
     parser.add_argument("-i", "--ignorecase", action="store_true", help="ignore case.")
 
